3150c4a40ea8fbc2cd197b3ee1c4a52f8a248a7d
Kent Liu <kent.liu@rschooltoday.com>
2024-12-25 15:34:12 -0600

    refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="finish-sound",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[ 
         "playsound>=1.2.2",
